f23742ace89d60fbb384dea9c50b65d1234ecb5a
CorentinTrt <corentintruffaut@gmail.com>
2023-02-21 14:40:14 -0500

    Reset content

--- src/api/string/controllers/string.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * string controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::string.string');
--- src/api/string/routes/string.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * string router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::string.string');
